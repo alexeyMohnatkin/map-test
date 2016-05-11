1e9986126456334f3600c8fa35ba9955d111c011
dizzy <alexey.mohnatkin@saydanke.ru>
2016-05-12 01:24:17 +0300

    Update 2016-05-11T22:24:04.567Z

---
+++ assets/scripts/0.756fadb1cfeba70ff67d.hot-update.js
@@ -0,0 +1,112 @@
+webpackHotUpdate(0,{
+
+/***/ 3:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _jquery = __webpack_require__(4);
+	
+	var _jquery2 = _interopRequireDefault(_jquery);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	var YandexRoute = function () {
+		function YandexRoute(layer) {
+			_classCallCheck(this, YandexRoute);
+	
+			console.log(layer._yandex);
+			this.map = layer._yandex;
+		}
+	
+		_createClass(YandexRoute, [{
+			key: 'buildRoute',
+			value: function buildRoute(points) {
+				if (points.length != 3) {
+					return;
+				}
+				this.points = points;
+				ymaps.ready(this._buildRoute.call(this));
+			}
+		}, {
+			key: '_buildRoute',
+			value: function _buildRoute() {
+	
+				var myMap = this.map;
+				console.log('building a route');
+	
+				// Добавим на карту схему проезда
+				// от улицы Крылатские холмы до станции метро "Кунцевская"
+				// через станцию "Молодежная" и затем до станции "Пионерская".
+				// Точки маршрута можно задавать 3 способами:
+				// как строка, как объект или как массив геокоординат.
+				ymaps.route(this.points).then(function (route) {
+					myMap.geoObjects.add(route);
+	
+					console.log(route);
+					// Зададим содержание иконок начальной и конечной точкам маршрута.
+					// С помощью метода getWayPoints() получаем массив точек маршрута.
+					// Массив транзитных точек маршрута можно получить с помощью метода getViaPoints.
+					var points = route.getWayPoints(),
+					    lastPoint = points.getLength() - 1;
+					// Задаем стиль метки - иконки будут красного цвета, и
+					// их изображения будут растягиваться под контент.
+					// points.options.set('preset', 'twirl#redStretchyIcon');
+	
+					// Задаем контент меток в начальной и конечной точках.
+					points.get(0).properties.set('iconContent', 'Точка отправления');
+					points.get(lastPoint).properties.set('iconContent', 'Точка прибытия');
+	
+					// Проанализируем маршрут по сегментам.
+					// Сегмент - участок маршрута, который нужно проехать до следующего
+					// изменения направления движения.
+					// Для того, чтобы получить сегменты маршрута, сначала необходимо получить
+					// отдельно каждый путь маршрута.
+					// Весь маршрут делится на два пути:
+					// 1) от улицы Крылатские холмы до станции "Кунцевская";
+					// 2) от станции "Кунцевская" до "Пионерская".
+	
+					var moveList = 'Трогаемся,</br>',
+					    way,
+					    segments;
+					// Получаем массив путей.
+					for (var i = 0; i < route.getPaths().getLength(); i++) {
+						way = route.getPaths().get(i);
+						segments = way.getSegments();
+						for (var j = 0; j < segments.length; j++) {
+							var street = segments[j].getStreet();
+							moveList += 'Едем ' + segments[j].getHumanAction() + (street ? ' на ' + street : '') + ', проезжаем ' + segments[j].getLength() + ' м.,';
+							moveList += '</br>';
+						}
+					}
+					moveList += 'Останавливаемся.';
+					// Выводим маршрутный лист.
+					(0, _jquery2.default)('#list').append(moveList);
+				}, function (error) {
+					alert('Возникла ошибка: ' + error.message);
+				});
+			}
+			// return buildRoute(domNode)
+	
+		}]);
+	
+		return YandexRoute;
+	}();
+	
+	// export default YandexRoute;
+	
+	
+	exports.default = YandexRoute;
+
+/***/ }
+
+})
+//# sourceMappingURL=0.756fadb1cfeba70ff67d.hot-update.js.map
---
+++ assets/scripts/0.a54dfbce51b19438aaf3.hot-update.js
@@ -0,0 +1,123 @@
+webpackHotUpdate(0,[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var _lodash = __webpack_require__(1);
+	
+	var _lodash2 = _interopRequireDefault(_lodash);
+	
+	var _yandex = __webpack_require__(3);
+	
+	var _yandex2 = _interopRequireDefault(_yandex);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	/*
+	$(() => {
+		svg4everybody();
+	});
+	*/
+	
+	// var L = require('leaflet');
+	// const map = L.map('map').setView([55.756, 37.627], 13);
+	
+	// import svg4everybody from 'svg4everybody';
+	// import $ from 'jquery';
+	var map = new L.Map('map', { center: new L.LatLng(55.756, 37.627), zoom: 13, zoomAnimation: false });
+	
+	var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+	var yndx = new L.Yandex();
+	
+	// const ytraffic = new L.Yandex("null", {traffic:true, opacity:0.8, overlay:true});
+	
+	// map.addLayer(ytraffic);
+	
+	var group = L.FeatureGroup.loadEvents([osm, yndx]);
+	var YR = void 0,
+	    osmContainer = void 0,
+	    yndxContainer = void 0;
+	
+	group.on({
+		loading: function loading() {
+			console.log('The layer group just fired the "loading" event!');
+		},
+		load: function load() {
+			console.log('The layer group just fired the "loaded" event!');
+	
+			// какая-то жесть
+			// console.log(yndx);
+			setTimeout(function () {
+				YR = new _yandex2.default(yndx);
+			}, 4000);
+			// createYaObj(yndx)
+		}
+	});
+	group.addTo(map);
+	
+	// map.addLayer(yndx);
+	// map.addLayer(osm);
+	map.addControl(new L.Control.Layers({ 'OSM': osm, "Yandex": yndx }
+	// {"Traffic":ytraffic}
+	));
+	
+	// console.log(yndx.prototype);
+	/*L.Class.prototype.onAdd.call(yndx, function() {
+			// console.log('123');
+		}
+	);*/
+	
+	var markers = [],
+	    markerNames = ["A", "B", "C"];
+	
+	map.on('click', function (e) {
+	
+		if (markers.length == 3) return;
+	
+		var marker = L.marker(e.latlng, { draggable: true });
+		marker.addTo(map).bindPopup(markerNames.shift()).openPopup();
+	
+		marker.on('dragend', refreshMarkers);
+	
+		markers.push(marker);
+	
+		// if(markers.length === 3){
+		// YR.markers = markers.map( ({_latlng}) => {return [_latlng.lat, _latlng.lng]} );
+		buildRoute();
+		// }
+	});
+	
+	var refreshMarkers = function refreshMarkers() {
+		// if(markers.length === 3){
+		buildRoute();
+		// }
+		// console.log(markers);
+		/*_.each(markers, (marker, index) => {
+	 	console.log(index);
+	 	// YR.markers = markers;
+	 	// marker.dragend(()=>);
+	 });*/
+	};
+	
+	var buildRoute = function buildRoute() {
+		if (markers.length !== 3) {
+			console.warn('3 points required');
+			return;
+		}
+	
+		// YR.markers = markers.map( ({_latlng}) => {return [_latlng.lat, _latlng.lng]} );
+		// YandexRoute.buildRoute(yndx._map);
+		// let YR = new YandexRoute(yndx._yandex);
+		YR.buildRoute(markers.map(function (_ref) {
+			var _latlng = _ref._latlng;
+			return [_latlng.lat, _latlng.lng];
+		}));
+		// console.log(yndx);
+	};
+	
+	// console.log(YandexRoute);
+
+/***/ }
+])
+//# sourceMappingURL=0.a54dfbce51b19438aaf3.hot-update.js.map
